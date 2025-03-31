87646e2be5348eef49744d128ad9141cca47cd9d
mapleZhu <12211626@mail.sustech.edu.cn>
2025-03-31 14:53:11 +0800

    database

--- IDEframework/settings.py
+++ IDEframework/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'IDEframework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'maplebase',
+        'USER': 'maplezhu',
+        'PASSWORD': 'gcjjlHgideaNMlD0',
+        'HOST': 'mysql2.sqlpub.com',
+        'PORT': '3307',
     }
 }
 
