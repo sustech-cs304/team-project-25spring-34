6d4f6cfd646d64e551f42270794c0d65768e4754
Tongxin Wu <160842831+12345wtx@users.noreply.github.com>
2025-04-13 02:30:05 +0800

    Add loadingPDF

--- group_learn/urls.py
+++ group_learn/urls.py
@@ -5,7 +5,11 @@ app_name = 'group_learn'
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:room_name>/", views.room, name="room"),
     path("save_annotations/", views.save_annotations, name="save_annotations"),
     path("get_annotations/", views.get_annotations, name="get_annotations"),
+    path("get_pdf/", views.get_pdf, name="get_pdf"),
+    path("get_room_pdfs/", views.get_room_pdfs, name="get_room_pdfs"),
+    path('get_current_pdf/', views.get_current_pdf, name='get_current_pdf'),
+    path('set_current_pdf/', views.set_current_pdf, name='set_current_pdf'),
+    path('serve_pdf/<str:pdf_id>/', views.serve_pdf, name='serve_pdf'),
 ]
